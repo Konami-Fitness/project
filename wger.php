507f50ead513e0f5f23b6a15b91a1f218a5104f1
teddy-clark <teddyc21799@gmail.com>
2020-12-01 23:52:34 -0800

    working wger.php

--- wger.php
+++ wger.php
@@ -19,8 +19,12 @@ function callAPI($method, $url, $data) {
    // OPTIONS:
    curl_setopt($curl, CURLOPT_URL, $url);
    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-      'APIKEY: XYMry4e3VAgaNGLsOPTOVnJQZAtTwH2JnnNOsqAX',
+      'APIKEY: df1483f0003854703ed49501f3b91ca42e4f1105',
       'Content-Type: application/json',
+      //new
+      'Allow: GET, HEAD, OPTIONS',
+      'Vary: Accept',
+      'Authorization: Token df1483f0003854703ed49501f3b91ca42e4f1105',
    ));
    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
@@ -31,39 +35,34 @@ function callAPI($method, $url, $data) {
    return $result;
 }
 
-$get_data = callAPI('GET', 'https://api.nal.usda.gov/fdc/v1/foods/search?query=burger&api_key=XYMry4e3VAgaNGLsOPTOVnJQZAtTwH2JnnNOsqAX', false);
+$get_data = callAPI('GET', 'https://wger.de/api/v2/workoutlog/', false);
+// printf($get_data);
 $response = json_decode($get_data, true);
 
-if (!isset($response['response']['errors'])) {
-  $response['response']['errors'] ="";
-  $errors = $response['response']['errors'];
-} else {
-  $errors = $response['response']['errors'];
-}
-
-if (!isset($response['response']['data'][0])) {
-  $response['response']['data'][0] ="";
-  $errors = $response['response']['data'][0];
-} else {
-  $errors = $response['response']['data'][0];
-}
+// if (!isset($response['response']['errors'])) {
+//   $response['response']['errors'] ="";
+//   $errors = $response['response']['errors'];
+// } else {
+//   $errors = $response['response']['errors'];
+// }
+//
+// if (!isset($response['response']['data'][0])) {
+//   $response['response']['data'][0] ="";
+//   $errors = $response['response']['data'][0];
+// } else {
+//   $errors = $response['response']['data'][0];
+// }
 
 ?>
 <!doctype html>
 <html>
   <head>
-    <title>Konami Calculator</title>
+    <title>Konami Wger</title>
   </head>
   <body>
   <?php
-    foreach($response['foods'] as $row) {
-      printf($row['description'] . "<br>");
-      foreach($row['foodNutrients'] as $row2) {
-        if($row2['nutrientName'] == 'Energy' && $row2['unitName'] == 'KCAL' ) {
-          printf($row2['value'] . ' Calories' . '<br>');
-        }
-      }
-      printf("<br><br><br><br>");
+    foreach($response['results'] as $row) {
+      printf($row['id'] . "<br>" . $row['exercise'] . "<br><br><br><br><br>");
     }
   ?>
   </body>

a0ac2cc8fe9b9fc62798669f3ad887170a8cf65a
teddy-clark <teddyc21799@gmail.com>
2020-12-02 23:03:24 -0800

    working search not connected to db

--- searchwger.php
+++ searchwger.php
@@ -5,13 +5,12 @@ $username = "user";
 $password = "itws";
 
 // Create connection
-try {
-  $dbconn = new PDO('mysql:host=localhost;dbname=konamifitness',$username,$password);
-  $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
-
+// try {
+//   $dbconn = new PDO('mysql:host=localhost;dbname=konamifitness',$username,$password);
+//   $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//   echo "Connection failed: " . $e->getMessage();
+// }
 
 function callAPI($method, $url, $data) {
   $curl = curl_init();
@@ -30,97 +29,108 @@ function callAPI($method, $url, $data) {
    // OPTIONS:
    curl_setopt($curl, CURLOPT_URL, $url);
    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-      'APIKEY: XYMry4e3VAgaNGLsOPTOVnJQZAtTwH2JnnNOsqAX',
+      'APIKEY: df1483f0003854703ed49501f3b91ca42e4f1105',
       'Content-Type: application/json',
+      'Allow: GET, HEAD, OPTIONS',
+      'Vary: Accept',
+      'Authorization: Token df1483f0003854703ed49501f3b91ca42e4f1105',
    ));
    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
    // EXECUTE:
    $result = curl_exec($curl);
-   if(!$result){die("Connection Failure");}
+   if (!$result) {die("Connection Failure");}
    curl_close($curl);
    return $result;
 }
 
 
-function addFood($dbconn, $foodname) {
-  
+function addExercise($dbconn, $foodname) {
+
 }
 
 // Use the user's input as the search part of the url
 try {
   if (isset($_POST['op1']))  {
-    $searchphrase = $_POST['op1'];
+    $searchphrase = rawurlencode($_POST['op1']);
+    // echo rawurlencode($_POST['op1']);
+    $searchurl = 'https://wger.de/api/v2/exercise/?name=' . $searchphrase;
+
+    $get_data = callAPI('GET', $searchurl, false);
+    // printf($get_data);
+    $response = json_decode($get_data, true);
 
 
-    $searchurl = 'https://api.nal.usda.gov/fdc/v1/foods/search?query=' . 
-    $searchphrase . '&api_key=XYMry4e3VAgaNGLsOPTOVnJQZAtTwH2JnnNOsqAX';
 
+    // foreach($response['results'] as $exercise) {
+    //   if ($exercise['language'] == 2) {
+    //     printf($exercise['name'] . "<br>muscles:<br>");
+    //     foreach($exercise['muscles'] as $muscle) {
+    //       printf($muscle . "<br>");
+    //     }
+    //     printf("secondary muscles:<br>");
+    //     foreach($exercise['muscles_secondary'] as $muscle2) {
+    //       printf($muscle2 . "<br>");
+    //     }
+    //     printf("equipment:<br>");
+    //     foreach($exercise['equipment'] as $eqmt) {
+    //       printf($eqmt . "<br>");
+    //     }
+    //     printf("<br><br>");
+    //   }
+    // }
+    // if (is_array($response['results'])) printf('egg');
+    // else printf('bad egg');
+    //
+    foreach($response['results'] as $exercise) {
+    // $exercise = $response['results'];
 
-  $get_data = callAPI('GET', $searchurl, false);
-  $response = json_decode($get_data, true);
-
-  foreach($response['foods'] as $row) {
-      // print_r($row);
-    
       printf('<form method="post">
-        <input type="submit" name="addFood" class="button" value="' . $row['description'] . '">
+        <input type="submit" name="addExercise" class="button" value="' . $exercise['name'] . '">
       </form>');
 
+      printf('category: ' . $exercise['category'] . '<br>muscles: ');
 
-      foreach($row['foodNutrients'] as $row2) {
-        if($row2['nutrientName'] == 'Energy' && $row2['unitName'] == 'KCAL' ) {
-          printf($row2['value'] . ' Calories' . '<br>');
-          }
-
-        if($row2['nutrientName'] == 'Protein' && $row2['unitName'] == 'G' ) {
-          printf($row2['value'] . ' Protein' . '<br>');
-          }
-
-        if($row2['nutrientName'] == 'Carbohydrate, by difference' && $row2['unitName'] == 'G' ) {
-          printf($row2['value'] . ' Carbohydrate' . '<br>');
-          }
-
-        if($row2['nutrientName'] == 'Total lipid (fat)' && $row2['unitName'] == 'G' ) {
-          printf($row2['value'] . ' Fat' . '<br>');
-          }
+      foreach($exercise['muscles'] as $muscle) {
+        printf($muscle . ' ');
       }
-      printf('<br>');
+
+      if (sizeof($exercise['muscles_secondary']) > 0) {
+        printf('<br>secondary muscles: ');
+        foreach($exercise['muscles_secondary'] as $muscle2) {
+          printf($muscle2 . ' ');
+        }
+      }
+
+      if (sizeof($exercise['equipment']) > 0) {
+        printf('<br>equipment: ');
+        foreach($exercise['equipment'] as $eqmt) {
+          printf($eqmt . ' ');
+        }
+      }
+      printf('<br><br><br><br>');
     }
   }
 
-  if (isset($_POST['addFood']))  {
-    addFood($dbconn, $_POST['addFood']);
-  }
-} catch (PDOException $e) {
+  // by name, category, muscles, secondary muscles, equipment
+  // if (isset($_POST['addExercise']))  {
+  //   addFood($dbconn, $_POST['addExercise']);
+  // }
+}
+
+catch (PDOException $e) {
   echo $e->getMessage();
-}
-
-
-
-if (!isset($response['response']['errors'])) {
-  $response['response']['errors'] ="";
-  $errors = $response['response']['errors'];
-} else {
-  $errors = $response['response']['errors'];
-}
-
-if (!isset($response['response']['data'][0])) {
-  $response['response']['data'][0] ="";
-  $errors = $response['response']['data'][0];
-} else {
-  $errors = $response['response']['data'][0];
 }
 
 ?>
 <!doctype html>
 <html>
   <head>
-    <title>Konami Calculator</title>
+    <title>Exercises</title>
   </head>
   <body>
-    <form method="post" action="apisearchtest.php">
-      <label for="name">Search for a food: </label></br>
+    <form method="post" action="searchwger.php">
+      <label for="name">Search for an exercise: </label></br>
       <input type="text" name="op1" id="name" value="" />
     </form>
   </body>

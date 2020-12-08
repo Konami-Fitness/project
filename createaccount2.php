41bb9a3e779466be65d75b42d70cc5181e4491e9
shaneriggs <61204126+shaneriggs@users.noreply.github.com>
2020-12-08 14:06:07 -0500

    Update createaccount2.php

--- createaccount2.php
+++ createaccount2.php
@@ -1,7 +1,7 @@
 <?php
   $db_host = "localhost";
-  $db_user = "user";
-  $db_pass = "itws";
+  $db_user = "root";
+  $db_pass = "websys7";
   $db_name = "konami";
   $con = new mysqli($db_host, $db_user, $db_pass, $db_name);
 

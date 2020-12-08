4b09a0464c3798d520d70fc4043c32314e181c83
shaneriggs <61204126+shaneriggs@users.noreply.github.com>
2020-12-08 05:38:14 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -1,39 +1,4 @@
-<?php
-	session_start();
-	$db_host = "localhost";
-	$db_user = "user";
-	$db_pass = "itws";
-	$db_name = "konami";
 
-	if ( ! empty( $_POST ) ) {
-    if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
-        // Getting submitted user data from database
-        $con = new mysqli($db_host, $db_user, $db_pass, $db_name);
-        $stmt = $con->prepare("SELECT * FROM users WHERE username = ?");
-        $stmt->bind_param('s', $_POST['username']);
-        $stmt->execute();
-        $result = $stmt->get_result();
-    	$user = $result->fetch_object();
-
-
-    	// Verify user password and set $_SESSION
-    	if ( $_POST['password'] == $user->password  ) {
-    		$_SESSION['userid'] = $user->userID;
-    		$_SESSION['password'] = $user->password;
-    		$_SESSION['username'] = $user->username;
-    		$_SESSION['fname'] = $user->firstname;
-    		$_SESSION['lname'] = $user->lastname;
-    		$_SESSION['age'] = $user->age;
-    		$_SESSION['gendernum'] = $user->sex;
-    		$_SESSION['height'] = $user->height;
-    		$_SESSION['heightunit'] = $user->heightbin;
-    		$_SESSION['weight'] = $user->weight;
-    		$_SESSION['weightunit'] = $user->weightbin;
-    		$_SESSION['goalnum'] = $user->calorieplan;
-    	}
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html>
@@ -65,7 +30,7 @@
     </div>
 		<div class="loginbox">
 			<p>Sign in with your Konami Fitness account</p>
-			<form action="login.php" method="post">
+			<form action="login2.php" method="post">
 				<label for="email">Username</label><br>
 			  <input type="text" name="username" placeholder="Enter username" required><br>
 				<label for="password">Password</label><br>
